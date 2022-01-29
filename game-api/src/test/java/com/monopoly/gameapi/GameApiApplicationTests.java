682f9c69303cbe0e5a623b6e3498488c5cc4aa30
Ren Chalissery <ren.chalissery@rubiconwater.com>
2022-01-29 22:25:25 +1100

    Added .idea

---
+++ game-api/src/test/java/com/monopoly/gameapi/GameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monopoly.gameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
